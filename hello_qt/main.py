6b96138de1c7833f2f37408d872d83b3c7f2e152
Javier <javierh0730@gmail.com>
2025-02-17 17:06:54 -0600

    17/02/2025

--- hello_qt/main.py
+++ hello_qt/main.py
@@ -1,4 +1,4 @@
-from PySide6.QtWidgets import QApplication, QWidget
+from PySide6.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel
 
 
 class MainWindow(QWidget):
@@ -6,9 +6,16 @@ class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
         self.resize(800,600)
+        self.setWindowTitle("Hello Qt")
         
+        self.__setup_ui()
         
+    def __setup_ui(self):
+        layout = QVBoxLayout()
+        label = QLabel("Hola Mundo")
+        layout.addWidget(label)
         
+        self.setLayout(layout)
         
 if __name__ == "__main__":
     app = QApplication()
