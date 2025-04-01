25464c36bf5d72381f0e053803c40ad9c947954b
Javier <javierh0730@gmail.com>
2025-04-01 16:23:02 -0600

    1/4/2025

--- hello_qt/main.py
+++ hello_qt/main.py
@@ -2,62 +2,56 @@ from PySide6.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QPushB
 
 
 class MainWindow(QWidget):
-    
     def __init__(self):
         super().__init__()
-        self.resize(800,600)
-        self.setWindowTitle("hello qt")
-        
+        self.resize(800, 600)
+        self.setWindowTitle("Hello QT")
+
         self.__setup_ui()
-        
-    def __on__submit(self):
-        nombre= self.nombre_input.text()
-        print(nombre)
-        self.nombre_input.clear()
+    
+    def __on_submit(self):
+        nombre =self.nombre_imput.text()
+        self.nombre_imput.clear()
         self.label.setText(nombre)
-        
-        num = self.num_input.value()
+
+        num = self.num_imput.value()
         print(num)
-        self.num_input.setValue(0)
-        
+        self.num_imput.setValue(0)
+
+        label = QLabel(nombre)
+
         layout = self.layout()
-        
-        label =QLabel(nombre)
         layout.addWidget(label)
-        
+
         print(layout)
-        
-      
-        
-       
-        
-        
+    
+    
+
     def __setup_ui(self):
         layout = QVBoxLayout()
         self.label = QLabel("Hola Mundo")
+       
+        self.nombre_imput = QLineEdit()
+        self.nombre_imput.returnPressed.connect(self.__on_submit)
+
+        btn_1 = QPushButton("Imprimir Texto")
+        btn_1.clicked.connect(self.__on_submit)
         
-        self.nombre_input = QLineEdit()
-        self.nombre_input.returnPressed.connect(self.__on__submit)
+        self.num_imput = QSpinBox()
         
-        btn_1 = QPushButton("imprimir texto")
-        btn_1.clicked.connect(self.__on__submit)
-        
-        self.num_input = QSpinBox()
-        
-        
-        
-        
+
         layout.addWidget(self.label)
-        layout.addWidget(self.nombre_input)
-        layout.addWidget(self.num_input)
+        layout.addWidget(self.nombre_imput)
+        layout.addWidget(self.num_imput)
         layout.addWidget(btn_1)
-        
-        
+ 
         self.setLayout(layout)
-        
-if __name__ == "__main__":
+
+
+
+if __name__ == '__main__':
     app = QApplication()
-    
     main_window = MainWindow()
     main_window.show()
     app.exec()
+
